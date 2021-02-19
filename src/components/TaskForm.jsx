816bfab3ad8e92c4eeeaa514ec673cbb956f0330
Iden Han <idenhan129@gmail.com>
2021-02-20 01:04:29 +0900

    Implement ListContext

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ListContext } from "../context/ListContext";
 
 const TaskForm = () => {
+  const { addTask } = useContext(ListContext);
+
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+  const handleChange = e => {
+    setTitle(e.target.value);
+    console.log(title);
+  };
   return (
-    <form className="form">
+    <form onSubmit={handleSubmit} className="form">
       <input
         type="text"
         className="task-input"
         placeholder="Add Task"
         required
+        onChnage={handleChange}
       />
       <div className="buttons">
         <button type="submit" className="btn add-task-btn">
--- src/context/ListContext.jsx
+++ src/context/ListContext.jsx
@@ -1,16 +1,58 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import { v1 as uuid } from "uuid";
 
 export const ListContext = createContext();
 
 const ListContextProvier = props => {
-  const [tasks, setTasks] = useState([
-    { title: "Drink Enough Water", id: 1 },
-    { title: "Eat Enough Vegetables", id: 2 },
-    { title: "Code to make the world better", id: 3 },
-  ]);
+  const initialState = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  const [tasks, setTasks] = useState(initialState);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const [editItem, setEditItem] = useState(null);
+
+  const addTask = title => {
+    setTasks([...tasks, { title, id: uuid() }]);
+  };
+
+  const removeTask = id => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  const clearList = () => {
+    setTasks([]);
+  };
+
+  const findTask = id => {
+    const item = tasks.find(task => task.id === id);
+
+    setEditItem(item);
+  };
+
+  const editTask = (title, id) => {
+    const newTasks = tasks.map(task => (task.id === id ? { title, id } : task));
+
+    console.log(newTasks);
+
+    setTasks(newTasks);
+    setEditItem(null);
+  };
 
   return (
-    <ListContext.Provider value={{ tasks }}>
+    <ListContext.Provider
+      value={{
+        tasks,
+        addTask,
+        removeTask,
+        clearList,
+        findTask,
+        editTask,
+        editItem,
+      }}
+    >
       {props.children}
     </ListContext.Provider>
   );
